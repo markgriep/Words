90079059fa06ed5548fa450e5415d0a97efaccd5
megriep <99667134+megriep@users.noreply.github.com>
2022-04-29 11:23:33 -0500

    Initial Code

---
+++ Words/Words/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			var x = System.IO.File.ReadAllLines(@"C:\temp\Wordlist-5k.txt");
+
+			var z = x.Where(v => v.Length < 6 && v.Length > 2);
+
+			//foreach (var ww in z)
+			//{
+			//Console.WriteLine(ww);
+
+			//}
+
+			System.IO.File.AppendAllLines(@"C:\temp\Wordlist-3-5.txt", z);
+
+			Console.WriteLine("Done");
+			Console.ReadKey();
+
+		}
+	}
+}
