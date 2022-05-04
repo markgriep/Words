bda9e8638d8155cc2ae5bf7a4383e71dc92c4b81
Mark Griep <markgriep@users.noreply.github.com>
2022-05-04 09:45:05 -0500

    added mac OS stuff

--- Words/Words/Program.cs
+++ Words/Words/Program.cs
@@ -8,13 +8,16 @@ namespace Words
         static void Main(string[] args)
         {
 			//var x = System.IO.File.ReadAllLines(@"C:\temp\Wordlist-5k.txt");
-			var x = System.IO.File.ReadAllLines(@".\wordlists\Wordlist-5k.txt");
+			//var x = System.IO.File.ReadAllLines(@".\wordlists\Wordlist-5k.txt");
+
+			var x = System.IO.File.ReadAllLines(@"/Users/markgriep/Desktop/Wordlist-5k.txt");
+
 
 			//var z = x.Where(v => v.Length < 6 && v.Length > 4);
 
 
 			var w = from ff in x
-					where ff.Length > 4	&& ff.Length < 6	
+					where ff.Length > 3	&& ff.Length < 5	
 					orderby ff
 					select ff;
 
@@ -30,7 +33,8 @@ namespace Words
 
 			//System.IO.File.AppendAllLines(@"C:\temp\Wordlist-3-5.txt", z);
 			//System.IO.File.AppendAllLines(@"c:\temp\Wordlist-5.txt", w.Distinct());
-			System.IO.File.WriteAllLines(@"c:\temp\Wordlist-5.txt", w.Distinct());
+			System.IO.File.WriteAllLines(@"/Users/markgriep/Desktop/Fivers.txt", w.Distinct());
+			System.IO.File.WriteAllLines(@"/Users/markgriep/Desktop/fours.txt", w.Distinct());
 
 			Console.WriteLine("Done");
 			//Console.ReadKey();
